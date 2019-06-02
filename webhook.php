48ad2c69f39e1b8a9eb72a0c6a75323d774a587d
0819kst <tb.kst.ib@gmail.com>
2019-06-03 02:14:43 +0900

    it make better

--- webhook.php
+++ webhook.php
@@ -8,19 +8,45 @@ $json_object = json_decode($json_string);
  
 //取得データ
 $replyToken = $json_object->{"events"}[0]->{"replyToken"};        //返信用トークン
+$userId = $jsonObj->{"events"}[0]->{"source"}->{"userId"};//userIDの取得
 $message_type = $json_object->{"events"}[0]->{"message"}->{"type"};    //メッセージタイプ
 $message_text = $json_object->{"events"}[0]->{"message"}->{"text"};    //メッセージ内容
  
 //メッセージタイプが「text」以外のときは何も返さず終了
-if($message_type != "text") exit;
+if($message_type == "beacon"){
+ResponseLineText( $accessToken, $replyToken, "近づいてきました！！" );
+}
+
+elseif($message_type != "text") exit;
  
 //返信メッセージ
+elseif($message_type == "text"){
 $return_message_text = "「" . $message_text . "」じゃねーよｗｗｗ";
- 
+ }
 //返信実行
 sending_messages($accessToken, $replyToken, $message_type, $return_message_text);
-?>
-<?php
+
+//Beaconの場合のメッセージ返信処理
+function ResponseLineText($accessToken,$replyToken,$text){
+        $response_format_text = [
+          "type" => "text",
+          "text" => $text
+        ];
+        $post_data = [
+           "replyToken" => $replyToken,
+           "messages" => [$response_format_text]
+        ];
+        $ch = curl_init("https://api.line.me/v2/bot/message/reply");
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: 
+        application/json; charser=UTF-8','Authorization: Bearer ' . $accessToken));
+        $result = curl_exec($ch);
+        curl_close($ch);
+     }
+
 //メッセージの送信
 function sending_messages($accessToken, $replyToken, $message_type, $return_message_text){
     //レスポンスフォーマット
