1cd5404df753e285ff202ba242b2ee720d03ef10
0819kst <tb.kst.ib@gmail.com>
2019-06-03 02:20:00 +0900

    it make better

--- webhook.php
+++ webhook.php
@@ -1,33 +1,15 @@
 <?php
  
 $accessToken = '9Qo0ZYAb7I2E2Mvpo/kvH6WyiymhsDlbC6WNRR49h38RPEST1BikeT6JVWY6XBmF42VBissR7FWnu9z0QZpO//6LEhVNbyRTBziUhHYuAEQ7RgbihBPHUKux37o8kloEckUNV9TvU7RzYX21VE9g7QdB04t89/1O/w1cDnyilFU=';
- 
-//ユーザーからのメッセージ取得
-$json_string = file_get_contents('php://input');
-$json_object = json_decode($json_string);
- 
-//取得データ
-$replyToken = $json_object->{"events"}[0]->{"replyToken"};        //返信用トークン
-$userId = $jsonObj->{"events"}[0]->{"source"}->{"userId"};//userIDの取得
-$message_type = $json_object->{"events"}[0]->{"message"}->{"type"};    //メッセージタイプ
-$message_text = $json_object->{"events"}[0]->{"message"}->{"text"};    //メッセージ内容
- 
-//メッセージタイプが「text」以外のときは何も返さず終了
-if($message_type == "beacon"){
-ResponseLineText( $accessToken, $replyToken, "近づいてきました！！" );
-}
-
-elseif($message_type != "text") exit;
- 
-//返信メッセージ
-elseif($message_type == "text"){
-$return_message_text = "「" . $message_text . "」じゃねーよｗｗｗ";
- }
-//返信実行
-sending_messages($accessToken, $replyToken, $message_type, $return_message_text);
-
-//Beaconの場合のメッセージ返信処理
-function ResponseLineText($accessToken,$replyToken,$text){
+ $json_string = file_get_contents('php://input');
+    $jsonObj = json_decode($json_string);
+    $replyToken = $jsonObj->{"events"}[0]->{"replyToken"};     //ReplyToken取得
+    $userId = $jsonObj->{"events"}[0]->{"source"}->{"userId"};        //userId取得
+    $eventType = $jsonObj->{"events"}[0]->{"type"};  //typeの取得
+    if($eventType == "beacon"){
+      ResponseLineText( $accessToken, $replyToken, "僕との距離が近づいてきました！！" );
+    }
+    function ResponseLineText($accessToken,$replyToken,$text){
         $response_format_text = [
           "type" => "text",
           "text" => $text
@@ -46,32 +28,4 @@ function ResponseLineText($accessToken,$replyToken,$text){
         $result = curl_exec($ch);
         curl_close($ch);
      }
-
-//メッセージの送信
-function sending_messages($accessToken, $replyToken, $message_type, $return_message_text){
-    //レスポンスフォーマット
-    $response_format_text = [
-        "type" => $message_type,
-        "text" => $return_message_text
-    ];
- 
-    //ポストデータ
-    $post_data = [
-        "replyToken" => $replyToken,
-        "messages" => [$response_format_text]
-    ];
- 
-    //curl実行
-    $ch = curl_init("https://api.line.me/v2/bot/message/reply");
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-        'Content-Type: application/json; charser=UTF-8',
-        'Authorization: Bearer ' . $accessToken
-    ));
-    $result = curl_exec($ch);
-    curl_close($ch);
-}
-?>
+?> 
